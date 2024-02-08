3b8fc318a2cbfb53c65c499225676d1ad6ae910b
Artur Chudlia <artur.chudlya@transportexchangegroup.com>
2024-02-08 12:19:57 +0000

    performance

--- src/test/java/org/example/features/search/SearchByKeyWordsArray.java
+++ src/test/java/org/example/features/search/SearchByKeyWordsArray.java
@@ -1,20 +1,15 @@
 package org.example.features.search;
 
-import app.getxray.xray.junit.customjunitxml.annotations.XrayTest;
 import net.serenitybdd.junit.runners.SerenityRunner;
-import net.thucydides.core.annotations.Issue;
 import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Pending;
 import net.thucydides.core.annotations.Steps;
-
+import org.example.steps.serenity.EndUserSteps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
-import org.example.steps.serenity.EndUserSteps;
-
 @RunWith(SerenityRunner.class)
-public class SearchByKeywordStory {
+public class SearchByKeyWordsArray {
 
     @Managed(uniqueSession = true)
     public WebDriver webdriver;
@@ -23,194 +18,164 @@ public class SearchByKeywordStory {
     public EndUserSteps anna;
 
     @Test
-//    @XrayTest(key = "XRTMP-35")
-//    @Requirement("XPTO-2847")
-    public void searching_by_keyword_apple_should_display_the_corresponding_article() {
-        anna.is_the_home_page();
-        anna.looks_for("apple");
-        anna.should_see_definition("w4rg34g34g");
-
-    }
-
-    @Test
-    public void searching_by_keyword_banana_should_display_the_corresponding_article() {
-        anna.is_the_home_page();
-        anna.looks_for("pear");
-        anna.should_see_definition("An edible fruit produced by the pear tree, similar to an apple but typically elongated towards the stem.");
-    }
-
-    @Test
-    public void searching_by_ambiguious_keyword_should_display_the_disambiguation_page_NEW() {
+    public void testNewArrayTest_1() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-//    @XrayTest(key = "XRTMP-15")
-    public void verify_IF_OfferIsRetractedWhenBuyerHasNoSufficientFunds() {
+    public void testNewArrayTest_2() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_1() {
+    public void testNewArrayTest_3() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_2() {
+    public void testNewArrayTest_4() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_3() {
+    public void testNewArrayTest_5() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_4() {
+    public void testNewArrayTest_6() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_5() {
+    public void testNewArrayTest_7() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_6() {
+    public void testNewArrayTest_8() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_7() {
+    public void testNewArrayTest_9() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_8() {
+    public void testNewArrayTest_10() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_9() {
+    public void testNewArrayTest_11() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_10() {
+    public void testNewArrayTest_12() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_11() {
+    public void testNewArrayTest_13() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_12() {
+    public void testNewArrayTest_14() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_13() {
+    public void testNewArrayTest_16() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_14() {
+    public void testNewArrayTest_17() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_16() {
+    public void testNewArrayTest_18() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_17() {
+    public void testNewArrayTest_19() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_18() {
+    public void testNewArrayTest_20() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_19() {
+    public void testNewArrayTest_21() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_20() {
+    public void testNewArrayTest_22() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_21() {
+    public void testNewArrayTest_23() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_22() {
+    public void testNewArrayTest_24() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_23() {
+    public void testNewArrayTest_25() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_24() {
+    public void testNewArrayTest_26() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_25() {
+    public void testNewArrayTest_27() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
 
     @Test
-    public void testArray_26() {
+    public void testNewArrayTest_28() {
         anna.is_the_home_page();
         anna.looks_for("pineapple");
     }
-
-    @Test
-    public void testArray_27() {
-        anna.is_the_home_page();
-        anna.looks_for("pineapple");
-    }
-
-    @Test
-    public void testArray_28() {
-        anna.is_the_home_page();
-        anna.looks_for("pineapple");
-    }
-} 
+}
